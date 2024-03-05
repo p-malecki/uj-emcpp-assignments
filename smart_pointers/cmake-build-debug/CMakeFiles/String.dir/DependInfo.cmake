de98a0715878b7e1e0eac1654069a84210105e6d
p-malecki <p.malecki2002@gmail.com>
2024-03-05 14:41:05 +0100

    Add .idea to smart pointers

---
+++ smart_pointers/cmake-build-debug/CMakeFiles/String.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/pawel/Documents/studia/6semestr/CPP/emcpp-assignments/smart_pointers/Ex4_String.cpp" "CMakeFiles/String.dir/Ex4_String.cpp.obj" "gcc" "CMakeFiles/String.dir/Ex4_String.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
