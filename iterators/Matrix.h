57508753f09c9049559b36b08f94fc27741de3ee
Paweł Małecki <p.malecki2002@gmail.com>
2024-04-27 13:43:43 +0200

    Add iterators task2 done

--- iterators/Matrix.h
+++ iterators/Matrix.h
@@ -1,23 +1,24 @@
-#pragma include once
+#pragma once
 
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <stdexcept>
 using namespace std;
 
 template <typename T, size_t N, size_t M>
 class Matrix{
-
 	T data[N*M];
 
  public:
-    size_t numberOfRows() { return N; }
-	size_t numberOfColumns() { return M; }
-	
-	Matrix(int nrows=N, int ncols=M){
+    constexpr size_t numberOfRows() const noexcept { return N; }
+    constexpr size_t numberOfColumns() const noexcept { return M; }
+
+    constexpr Matrix(int /*nrows*/ = N, int /*ncols*/ = M) noexcept {
 	  std::fill_n(data, N*M, T{});
 	}
-    Matrix(const std::initializer_list<std::initializer_list<T>> & list){
+
+    Matrix(const std::initializer_list<std::initializer_list<T>> & list) {
 		T * p = data;
 		for(const auto & row: list){
 	  		T* p2 = std::copy(row.begin(), row.end(), p);
@@ -25,26 +26,30 @@ class Matrix{
 		}
     }
 
-	Matrix(const Matrix & m){
+	Matrix(const Matrix & m) noexcept {
 		std::copy_n(m.data, N*M, data);
 	}
-	
-	Matrix & operator= (const Matrix & m){
+
+	Matrix & operator= (const Matrix & m) noexcept {
 		if(&m != this){
 			std::copy_n(m.data, N*M, data);
 		}
 		return *this;
 	}
 
-	T& operator()(int i, int j) {
+    T& operator()(int i, int j) noexcept {
+        return data[(i-1)*M+j-1];
+    }
+
+	const T& operator()(int i, int j) const noexcept {
 	   return data[(i-1)*M+j-1];
 	}
 
-    friend Matrix operator+( Matrix & a, Matrix &b){
+    friend Matrix operator+(const Matrix & a, const Matrix &b) noexcept {
 		Matrix r;
-		for(int i = 1; i <= N; ++i){
-			for(int j = 1; j <= M; ++j){
-				r(i,j) = a(i,j) + b(i,j) ;
+		for(size_t i = 1; i <= N; ++i){
+			for(size_t j = 1; j <= M; ++j){
+				r(i,j) = a(i,j) + b(i,j);
 			}
 		}
 		return r;
@@ -53,13 +58,13 @@ class Matrix{
 };
 
 template <typename T, size_t N, size_t M>
-void printMatrix(Matrix<T,N,M> & m, int width = 10){
-	for(int i = 1; i <= m.numberOfRows(); ++i){
-		for(int j = 1; j <= m.numberOfColumns(); ++j){
-			if(j != 1) 
-				cout << " ";
-			cout << setw(width) <<  m(i,j) ;
-		}
-		cout << endl;
-	}
+void printMatrix(const Matrix<T,N,M> & m, int width = 10) noexcept {
+    for(size_t i = 1; i <= m.numberOfRows(); ++i) {
+        for(size_t j = 1; j <= m.numberOfColumns(); ++j) {
+            if(j != 1)
+                cout << " ";
+            cout << setw(width) <<  m(i,j);
+        }
+        cout << endl;
+    }
 }
